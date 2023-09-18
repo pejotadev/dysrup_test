8a29e25df7926eeec2f1451bf8770349c7d98850
Pedro José NPS <pedrojosepaiva@outlook.com>
2023-09-18 14:14:33 -0300

    wpp contact

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,6 +16,7 @@ const Header = () => {
             </Link>
             <div className="flex items-center lg:order-2">
                 <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800" onClick={logout}>Log out</a>
+                <a data-v-b87517e0="" target="_blank" href="https://api.whatsapp.com/send?phone=55(31) 97134-7237&amp;text=Ol%C3%A1%20Pedro,%20tudo%20bem?%20%F0%9F%98%89%20%0Gostamos%20muito%20do%20seu%20*Perfil!*,%20"><button data-v-b87517e0="" name="button" class="vs-component vs-button d-inline vs-button-success vs-button-gradient isActive includeIcon includeIconOnly vs-radius" data-original-title="null" aria-describedby="tooltip_95vvs92kk4"><i class="vs-icon notranslate icon-scale vs-button--icon  fa fa-whatsapp null" style="order: 0; margin-right: 0px; margin-left: 0px;"></i><span class="vs-button-linex" style="top: auto; bottom: -2px; left: 50%; transform: translate(-50%);"></span></button></a>
                 
             </div>
         </div>
